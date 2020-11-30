98bf55a3a0f6a25e15ceb210722483251dd1c3bf
Icestab <dengyue35@qq.com>
2020-11-30 20:14:28 +0800

    完成可拖拽div

--- js-demo/main.js
+++ js-demo/main.js
@@ -1,5 +1,29 @@
 var div = document.getElementById('d')
-document.onmousedown = (e) => {
-    div.style.top = e.clientY + 'px'
-    div.style.left = e.clientX + 'px'
+
+var flag = false
+var diffX, diffY = 0
+var startX, startY = 0
+div.onmousedown = (e) => {
+    startX = e.clientX
+    startY = e.clientY
+    flag = true
+}
+document.onmouseup = (e) => {
+
+    flag = false
+
+}
+document.onmousemove = (e) => {
+    if (flag) {
+        diffX = e.clientX - startX
+        diffY = e.clientY - startY
+        let nowX = parseInt(div.style.left) || 0
+        let nowY = parseInt(div.style.top) || 0
+        let goalX = (nowX + diffX) > 0 ? (nowX + diffX) : 0
+        let goalY = (nowY + diffY) > 0 ? (nowY + diffY) : 0
+        div.style.left = goalX + 'px'
+        div.style.top = goalY + 'px'
+        startX = e.clientX
+        startY = e.clientY
+    }
 }
